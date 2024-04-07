9cfbf697743a3e0c29aeec8aa1f53eca2a82a7ea
Wediah <100675006+Wediah@users.noreply.github.com>
2024-04-07 23:23:27 +0000

    adds mmc for departments

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function departments()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
